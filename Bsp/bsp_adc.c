ebd693add8028d234cf54e3b27e5232775e48ca4
Weistr <1561267515@qq.com>
2023-12-21 12:47:08 +0800

    date

--- Bsp/bsp_adc.c
+++ Bsp/bsp_adc.c
@@ -3,7 +3,7 @@
 #include "bsp_adc.h"
 #include "filter.h"
 #include "stdbool.h"
-
+#include "stm32f3xx_ll_adc.h"
 
 
 /* Exported types ------------------------------------------------------------*/
@@ -20,19 +20,21 @@ uint16_t adcBufFilted[4];
 
 uint16_t battVot_mv ,bv_mv,pv_mv,cref_mv;
 int16_t pv_ma,pi_ma,bi_ma;
-const uint16_t vref_mV = 1200;
 
+uint16_t vrefmv;
 uint8_t cnt1=0,cnt2=0,cnt3=50;
 /* END-------------------------------------------------------------------------*/
 
 /* Private define ---------------------------------------------------------*/
 
-#define vref_adc u16adc_buf[3] //stm32内部基准电压
+
 #define bv_adc u16adc_buf[1] //buck输出电压
 #define pv_adc u16adc_buf[2] //buck输入电压
+#define vref_adc u16adc_buf[3]
 #define bi_adc u16adc2_buf[2] //buck输出电流
 #define pi_adc u16adc2_buf[1] //buck输入电流
 #define cref_adc u16adc2_buf[0] //电流运放基准电压
+#define vrefmv 1242
 /* END-------------------------------------------------------------------------*/
 
 
@@ -42,6 +44,9 @@ void adc_init()
 {
 	HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
 	HAL_ADCEx_Calibration_Start(&hadc2, ADC_SINGLE_ENDED);
+	HAL_ADC_Start_DMA(&hadc1,(uint32_t*)&u16adc_buf[0],4);
+	HAL_ADC_Start_DMA(&hadc2,(uint32_t*)&u16adc2_buf[0],3);
+
 }
 
 
@@ -49,27 +54,18 @@ void adc_scan()
 {
 
 	
-	/*
-	for(i=0;i<4;i++)
-	{
-		adcBufFilted[i] =XIIR_filter(0.8,u16adc_buf[i],i);
-	}
-	for(i=0;i<3;i++)
-	{
-		adcBufFilted[i] =XIIR_filter(0.8,u16adc2_buf[i],i);
-	}	
-	*/
-//vref
+
 
 //buck out votage VO
 	
-	cref_mv = (int32_t)u16adc2_buf[0] * vref_mV / vref_adc;
+	float VrefRat = (float)vrefmv/vref_adc;
+	cref_mv = (float)u16adc2_buf[0] * VrefRat;
 
-	bv_mv = (int32_t)u16adc_buf[1] * vref_mV / vref_adc * 2.0f;
-	pv_mv = (int32_t)u16adc_buf[2] * vref_mV / vref_adc * 5.65116f;
+	bv_mv = (float)u16adc_buf[1] * VrefRat * 1.99893f;
+	pv_mv = (float)u16adc_buf[2] * VrefRat * 5.664175f;
 
-	bi_ma = (int32_t)(bi_adc-cref_adc) * vref_mV / vref_adc * 2;
-	pi_ma = (int32_t)(pi_adc-cref_adc) * vref_mV / vref_adc * 2;
+	bi_ma = (float)(bi_adc-cref_adc) * VrefRat * 2;
+	pi_ma = (float)(pi_adc-cref_adc) * VrefRat * 2;
 	
 	HAL_ADC_Start_DMA(&hadc1,(uint32_t*)&u16adc_buf[0],4);
 	HAL_ADC_Start_DMA(&hadc2,(uint32_t*)&u16adc2_buf[0],3);
